9a0b865f1e38afee17cff6d1220008ac8cdd7b54
Edward Lancaster <edwardlancaster@pop-os.localdomain>
2018-08-19 11:05:51 +0200

    Add more verbose feedback to setup and install scripts

--- install.sh
+++ install.sh
@@ -10,51 +10,89 @@ if [ "$EUID" -ne 0 ]
 fi
 
 # Fully upgrades the system
-echo "Updating apt repositories..."
 apt -qq update
-echo "Upgrading software..."
+echo -e $TEXT_RED_B='\e[1;31m'
+echo "Updated apt repositories"
+echo -e $TEXT_RESET
+
 apt -qq upgrade -y
+echo -e $TEXT_RED_B='\e[1;31m'
+echo "Updated software"
+echo -e $TEXT_RESET
 
 # Installs prerequired packages
+echo -e $TEXT_RED_B='\e[1;31m'
 echo "Installing curl, gnupg, build-essential and dirmngr"
+echo -e $TEXT_RESET
 apt -qq install -y curl gnupg build-essential dirmngr
 
 # Fetches key from server
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+echo -e $TEXT_RED_B='\e[1;31m'
+echo "Key fetched"
+echo -e $TEXT_RESET
 
 # Fetches RVM from server
 curl -sSL https://get.rvm.io | bash -s stable
+echo -e $TEXT_RED_B='\e[1;31m'
+echo "RVM installed"
+echo -e $TEXT_RESET
 
 # Sets rvm source file
 source /etc/profile.d/rvm.sh
+echo -e $TEXT_RED_B='\e[1;31m'
+echo "Source has been set"
+echo -e $TEXT_RESET
 
 # Adds current user to rvm group
 usermod -a -G rvm root
+echo -e $TEXT_RED_B='\e[1;31m'
+echo "Root user added to rvm group"
+echo -e $TEXT_RESET
 
 # Installs the required version of Ruby
 rvm install ruby-2.5.1
-
+echo -e $TEXT_RED_B='\e[1;31m'
+echo "Ruby 2.5.1 Installed"
+echo -e $TEXT_RESET
 # Sets installed version as default
 rvm --default use ruby-2.5.1
+echo -e $TEXT_RED_B='\e[1;31m'
+echo "Ruby 2.5.1 set as default"
+echo -e $TEXT_RESET
 
 # Installs the bundler gem without documentation
 gem install bundler --no-rdoc --no-ri
+echo -e $TEXT_RED_B='\e[1;31m'
+echo "Bundler installed"
+echo -e $TEXT_RESET
 
 # Installs nodejs for Rails compatability
 echo "Installing nodejs"
 apt -qq update && apt -qq install -y apt-transport-https ca-certificates && curl --fail -ssL -o setup-nodejs https://deb.nodesource.com/setup_8.x && bash setup-nodejs && sudo apt-get install -y nodejs build-essential
-
+echo -e $TEXT_RED_B='\e[1;31m'
+echo "nodejs installed"
+echo -e $TEXT_RESET
 # Gets keys for passenger
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recvkeys 561F9B9CAC40B2F7
-
+echo -e $TEXT_RED_B='\e[1;31m'
+echo "Passenger keys received"
+echo -e $TEXT_RESET
 # Adds the repo to the sources list and refreshes them
 sh -c 'echo deb https://oss-binaries.phusionpassenger.com/apt/passenger stretch main > /etc/apt/sources.list.d/passenger.list'
-echo "Updating apt repositories..."
+echo -e $TEXT_RED_B='\e[1;31m'
+echo "Repo added to apt list"
+echo -e $TEXT_RESET
 apt -qq update
+echo -e $TEXT_RED_B='\e[1;31m'
+echo "Apt repositories updated"
+echo -e $TEXT_RESET
 
 # Installs the Passenger and Apache modules
-echo "Installing passenger + apache modules"
 apt -qq install -y libapache2-mod-passenger
+echo -e $TEXT_RED_B='\e[1;31m'
+echo "Passenger + Apache moules installed"
+echo -e $TEXT_RESET
 
 # Enable passenger mod (just in case)
 a2enmod passenger
--- setup.sh
+++ setup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+TEXT_RESET='\e[0m'
+TEXT_YELLOW='\e[0;33m'
+TEXT_RED_B='\e[1;31m'
+
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
@@ -9,24 +14,58 @@ cd /
 # Adds dedicated user for safety reasons
 useradd -m -p SiYnqBkxiqWvo bkstool
 bash
+echo -e $TEXT_RED_B='\e[1;31m'
+echo "User has been added"
+echo -e $TEXT_RESET
 source /etc/profile.d/rvm.sh
+echo -e $TEXT_RED_B='\e[1;31m'
+echo "Source set"
+echo -e $TEXT_RESET
 usermod -a -G rvm bkstool
+echo -e $TEXT_RED_B='\e[1;31m'
+echo "User added to group rvm"
+echo -e $TEXT_RESET
 
 # Makes folder to hold webapp
 mkdir -p /var/www/bkstool
+echo -e $TEXT_RED_B='\e[1;31m'
+echo "Folder created"
+echo -e $TEXT_RESET
 chown bkstool: /var/www/bkstool
+echo -e $TEXT_RED_B='\e[1;31m'
+echo "Permission given to new user"
+echo -e $TEXT_RESET
 
 # Switches to created folder and downloads repo
 su - bkstool
+echo -e $TEXT_RED_B='\e[1;31m'
+echo "Switched to new user"
+echo -e $TEXT_RESET
 source /etc/profile.d/rvm.sh
 cd /var/www
+echo -e $TEXT_RED_B='\e[1;31m'
+echo "changed to folder"
+echo -e $TEXT_RESET
 git clone https://bitbucket.org/elancaster/bks-tool
+echo -e $TEXT_RED_B='\e[1;31m'
+echo "Source cloned to folder"
+echo -e $TEXT_RESET
 
 # Install app dependencies
 rvm rvmrc warning ignore /var/www/bks-tool/Gemfile
 cd /bks-tool
+echo -e $TEXT_RED_B='\e[1;31m'
+echo "Changed to source folder"
+echo -e $TEXT_RESET
 bundle install --deployment --without development test
+echo -e $TEXT_RED_B='\e[1;31m'
+echo "Bundler installed"
+echo -e $TEXT_RESET
 exit
+exit
+echo -e $TEXT_RED_B='\e[1;31m'
+echo "Switched back to root user"
+echo -e $TEXT_RESET
 
 # Configures the apache
 touch /etc/apache2/sites-enabled/bkstool.conf
@@ -46,6 +85,16 @@ echo "      Options -MultiViews" >> bkstool.conf
 echo "    </Directory>" >> bkstool.conf
 echo "</VirtualHost>" >> bkstool.conf
 apache2ctl restart
+echo -e $TEXT_RED_B='\e[1;31m'
+echo "Apache configured and restarted"
+echo -e $TEXT_RESET
 
 apt -qq update
 apt -qq upgrade
+echo -e $TEXT_RED_B='\e[1;31m'
+echo "Sys update finished"
+echo -e $TEXT_RESET
+
+echo -e $TEXT_RED_B='\e[1;31m'
+echo "Installation complete! To test, go to athene.bks-campus.ch"
+echo -e $TEXT_RESET
