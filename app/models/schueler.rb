6b8639b5a0c1f735f60b2ab312b096e498f2f8c4
Edward Lancaster <s9362@bks-campus.ch>
2018-09-20 10:35:23 +0000

    Refactor upload code in model folder

--- app/models/schueler.rb
+++ app/models/schueler.rb
@@ -2,9 +2,11 @@ class Schueler < ApplicationRecord
   include Resetable
   require "csv"
   def self.import(file)
-    table = CSV.read(file.path, { headers: true, col_sep: ";" })
-
-    table.each do |row| # Generates password for each user and marks selection as nil
+    CSV.foreach(file.path, headers: true, col_sep: ";") do |row|
+      Schueler.create! row.to_hash
+    end
+    stud = Schueler.all
+    stud.each do |row|
       pass = 1.times.map{ 9999 + Random.rand(100000) }
       row["password"] = pass.join
       row["password_confirmation"] = pass.join
@@ -16,15 +18,6 @@ class Schueler < ApplicationRecord
       row["Received"] = false
     end
 
-    CSV.open(file.path, "w") do |f|
-      f << table.headers
-      table.each{ |row| f << row }
-    end
-
-    CSV.foreach(file.path, headers: true) do |row|
-      Schueler.create! row.to_hash
-    end
-    students = Schueler.all
     Schueler.all.order!(Name: :asc, Vorname: :desc)
   end
   validates :Name,     presence: true
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -4,15 +4,15 @@ class Teacher < ApplicationRecord
   def self.import(file)
     table = CSV.read(file.path, { headers: true, col_sep: ";" })
 
-    CSV.open(file.path, "w") do |f|
-      f << table.headers
-      table.each{ |row| f << row }
-    end
-
     CSV.foreach(file.path, headers: true) do |row|
       Teacher.create! row.to_hash
     end
-    teachers = Teacher.all
+    teac = Teacher.all
+    teac.each do |row|
+      row["Received"] = false
+    end
+
+    Teacher.all.order!(Name: :asc, Vorname: :desc)
   end
   validates :Name, presence: true
   validates :Number, uniqueness: { case_sensitive: false }
